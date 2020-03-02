30462ee8d300f67fe66902b841c470799b19f7d5
Kevin Su <kevinsu@outlook.co.nz>
2020-03-02 12:56:37 +1100

    Updates

---
+++ precache-manifest.a72db6fd6797d497b25f91f79633ab6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3651d557df8f359e6d733c2eaefe2602",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae767a36c3057d66aee5",
+    "url": "/static/css/main.9d8339a7.chunk.css"
+  },
+  {
+    "revision": "1911b28e5457b0728dfa",
+    "url": "/static/js/2.8b6d70ee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.8b6d70ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae767a36c3057d66aee5",
+    "url": "/static/js/main.8ddeff43.chunk.js"
+  },
+  {
+    "revision": "5e4329a62d70a624550b",
+    "url": "/static/js/runtime-main.9c68a72f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
